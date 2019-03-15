6ac1e7fb10610c82c6ea225c4cdb6c7d5df90514
SK <SK@DESKTOP-ODTOFJV>
2019-03-15 21:48:21 +0300

    test

--- index.php
+++ index.php
@@ -10,8 +10,7 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        
-        <?php
+             <?php
         // put your code here
         ?>
     </body>
